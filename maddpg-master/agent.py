269d649f06158e6a64cff038abe0e7006ca679a9
Dylan Perry <perryd37@students.rowan.edu>
2021-09-16 09:46:34 -0400

    initial commit for working from laptop

---
+++ maddpg-master/agent.py
@@ -0,0 +1,34 @@
+import numpy as np
+import torch
+import os
+from maddpg.maddpg import MADDPG
+
+
+class Agent:
+    def __init__(self, agent_id, args):
+        self.args = args
+        self.agent_id = agent_id
+        self.policy = MADDPG(args, agent_id)
+
+    def select_action(self, o, noise_rate, epsilon):
+        if np.random.uniform() < epsilon:
+            #print("!!!!!!!")
+            print(self.args.high_action)
+            u = np.random.uniform(-self.args.high_action, self.args.high_action, self.args.action_shape[self.agent_id])
+        else:
+            inputs = torch.tensor(o, dtype=torch.float32).unsqueeze(0)
+            print("!!!!!!!!!!!!!inpuits")
+            print(inputs)
+            pi = self.policy.actor_network(inputs).squeeze(0)
+            print("!!!!!!!!!!!pi")
+            print(pi)
+            #print('{} : {}'.format(self.agent_id, pi))
+            u = pi.cpu().numpy()
+            noise = noise_rate * self.args.high_action * np.random.randn(*u.shape)  # gaussian noise
+            u += noise
+            u = np.clip(u, -self.args.high_action, self.args.high_action)
+        return u.copy()
+
+    def learn(self, transitions, other_agents):
+        self.policy.train(transitions, other_agents)
+
---
+++ multiagent-particle-envs-master/multiagent/scenarios/smart_load.py
@@ -0,0 +1,151 @@
+import numpy as np
+from multiagent.core import World, Agent, Landmark
+from multiagent.scenario import BaseScenario
+
+
+
+
+class Scenario(BaseScenario):
+    def make_world(self):
+        world = World()
+
+        #Scenario Properties
+        num_agents = 2
+        num_adversaries = 0
+
+        world.dim_c = 3
+        #world.dim_p = None
+        world.collaborative = True
+
+        self.load = 0
+        self.peak = 500
+        self.energy_costs = []
+        self.comfort = 0
+        self.occupied = True
+        
+        #Generate Agents
+        world.agents = [Agent() for i in range(2)]
+
+        for i, agent in enumerate(world.agents):
+            if i == 0:
+                agent.name = 'Smart_Building'
+            elif i == 1:
+                agent.name = "Charging_Station"
+            agent.movable = False
+            agent.size = .1
+        
+
+        world.landmarks = [Landmark() for i in range(3)]
+        #Generate Landmarks
+        for i, landmark in enumerate(world.landmarks):
+            if i == 0:
+                landmark.name = "Load"
+                landmark.size = .2
+            elif i==1:
+                landmark.name = "energy cost"
+                landmark.size = .1
+            elif i ==2:
+                landmark.name = "comfort"
+                landmark.size = .1
+            landmark.collide = False
+            landmark.movable = False
+            landmark.boundary = False
+
+                
+
+        self.reset_world(world)
+        return world
+
+
+
+    def reward(self, agent, world):
+        if agent.name == "Smart_Building":
+            return self.smart_building_reward(agent, world)
+        elif agent.name == "Charging_Station":
+            return self.charging_station_reward(agent, world)
+        
+        return None
+        pass
+
+
+
+    def reset_world(self, world):
+        world.energy_costs = []
+        #filling out agent detail
+        for agent in world.agents:
+            if agent.name == "Smart_Building":
+                agent.energy = 1
+                agent.comfort = 10 
+                agent.color = np.array([0.5,0.5,0.5])
+                agent.state.p_pos = np.array([-.2,0.2])
+                agent.agent_callback = None
+                pass
+            elif agent.name == "Charging_Station":
+                agent.rate = 4
+                agent.occupied = True
+                agent.required = 72
+                agent.state.p_pos = np.array([-.0,0.2])
+                agent.color = np.array([0.8,0.5,0.8])
+                agent.agent_callback = None 
+                pass
+            agent.state.c = np.zeros(world.dim_c)
+            pass
+
+        #filling out landmark detail
+        for landmark in world.landmarks:
+            if landmark.name == "Load":
+                landmark.color = np.array([0.1+self.load/self.peak/0.9, 1-self.load/self.peak,0])
+                landmark.state.p_pos = np.array([-.3,-.5])
+                pass
+            elif landmark.name == "energy cost":
+                landmark.state.p_pos = np.array([0,-0.5])
+                landmark.color = np.array([0.8,0.5,0.8])
+                pass
+            elif landmark.name == "comfort":
+                landmark.state.p_pos = np.array([.2,-.5])
+                landmark.color = np.array([0.1,0.5,0.8])
+                pass
+
+    def smart_building_reward(self, agent, world):
+        reward = 0
+        self.comfort = agent.state.c[1] * 50
+        if self.occupied:
+            reward += self.comfort
+        self.load = 50 - agent.state.c[2]
+        reward -= self.load
+
+        return reward
+
+    def charging_station_reward(self, agent, world):
+        return 0
+        
+    def step(self, actions):
+        print("actions was callled!!!!!!")
+        print(actions)
+        print("!#!#!#!#!#!#!#!#!")
+        pass
+
+    def observation(self, agent, world):
+        for landmark in world.landmarks:
+            if landmark.name == "Load":
+                landmark.color = np.array([0.1+self.load/self.peak/0.9, 1-self.load/self.peak,0])
+
+
+        for agent in world.agents:
+            if agent.name == "Charging_Station":
+                return([agent.rate, agent.required, self.load])
+            elif agent.name  == "Smart_Building":
+                print("~~~~~~~~")
+                print(agent.state.c)
+                print("~~~~~~~~")
+                self.load+= agent.energy
+                return([agent.energy, agent.comfort, self.load])
+
+        return [0,0,0]
+
+    def smart_buildings(self, agent, world):
+        return [agent for agent in world.agents if (agent.name == "Smart_Building")]
+
+    def charging_stations(self, agent, world):
+        return [agent for agent in world.agents if (agent.name == "Charging_Station")]
+
