378872e616b982d75e3faac618f76cd656ff7f64
Dylan Perry <perryd37@students.rowan.edu>
2021-10-05 17:04:27 -0400

    comment

--- multiagent-particle-envs-master/multiagent/scenarios/smart_load.py
+++ multiagent-particle-envs-master/multiagent/scenarios/smart_load.py
@@ -3,6 +3,7 @@ from multiagent.core import World, Agent, Landmark
 from multiagent.scenario import BaseScenario
 import random
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 
@@ -13,16 +14,17 @@ class Scenario(BaseScenario):
         #Scenario Properties
         num_agents = 2
         num_adversaries = 0
-        self.states = [[],[]]
         world.dim_c = 1
         world.dim_p = 2
         world.collaborative = True
-
+        self.data_path = r'C:\Users\perry\Documents\SURP_research\DEEPQ_SURP\PJME_hourly.csv'
+        self.energy_costs = [1,1,1,2,2,3,4,5,6,7,7,7,10,10,10,10,9,8,8,8,4,3,3,2]
+        #self.random_data()
         self.load = 0
-        self.peak = 5
-        self.energy_costs = []
+        self.peak = 20
         self.comfort = 0
         self.occupied = True
+        self.occupation = [0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0]
         self.done = False
         self.time = 0
         
@@ -39,8 +41,9 @@ class Scenario(BaseScenario):
             agent.size = .1
         
 
-        world.landmarks = [Landmark() for i in range(3)]
+
         #Generate Landmarks
+        world.landmarks = [Landmark() for i in range(3)]
         for i, landmark in enumerate(world.landmarks):
             if i == 0:
                 landmark.name = "Load"
@@ -56,12 +59,11 @@ class Scenario(BaseScenario):
             landmark.boundary = False
 
                 
-
         self.reset_world(world)
         return world
 
 
-
+    #reward method for calling. Deliberates to specific reward functions
     def reward(self, agent, world):
         if agent.name == "Smart_Building":
             return self.smart_building_reward(agent, world)
@@ -78,6 +80,8 @@ class Scenario(BaseScenario):
         self.comfort = 0
         self.done = False
         world.energy_costs = []
+
+        
         #filling out agent detail
         for agent in world.agents:
             if agent.name == "Smart_Building":
@@ -114,25 +118,10 @@ class Scenario(BaseScenario):
                 landmark.color = np.array([0.1,0.5,0.8])
                 pass
 
-    def smart_building_reward(self, agent, world):
-        reward = 0
-        
-        if self.occupied:
-            reward += self.comfort
-        
-        reward -= agent.energy
-        reward -= self.load
-        return reward
-
-    def charging_station_reward(self, agent, world):
-        reward = 0
-        if agent.required == 0:
-            reward = 100 - agent.rate*10
-        else:
-            reward = -(agent.rate*2+(agent.required))
-        return reward - self.load
-        
-
+    '''
+    Main observation method. Same as OpenAI observation.
+    Obs - should be returning multiple 
+    '''
     def observation(self, agent, world):
         print("Observe - " + agent.name)
         print(self.load)
@@ -147,26 +136,64 @@ class Scenario(BaseScenario):
 
         if agent.name == "Charging_Station":
             self.states[0].append(agent.state.c)
-            self.load -= agent.rate
+            self.load -= agent.energy
             agent.rate= agent.state.c[0]* 5
+            agent.energy = agent.rate * self.energy_cost[self.time]
             if agent.required < 0:
                 agent.required = 0
             elif agent.required > 0 and agent.rate > agent.required:
                 agent.required = 0
             elif agent.required > 0 and agent.required > agent.rate:
                 agent.required -= agent.rate
-            self.load += agent.rate
+            self.load += agent.energy
             return([agent.rate, agent.required, self.load])
         elif agent.name  == "Smart_Building":
+            self.time += 1
             self.states[1].append(agent.state.c)
             self.load -= agent.energy
-            agent.energy = agent.state.c[0] * self.peak/2
+            agent.energy = agent.state.c[0] * self.energy_cost[self.time]
             self.load += agent.energy
             self.comfort = agent.energy * agent.comfort
             return([agent.energy, agent.comfort, self.load])
+
         
+    def smart_building_reward(self, agent, world):
+        reward = 0
+        
+        if self.occupied:
+            reward += self.comfort
+        
+        reward -= agent.energy
+        reward -= max(self.load, 0)
+        return reward
 
+    def charging_station_reward(self, agent, world):
+        reward = 0
+        if agent.required == 0:
+            reward = 100 - agent.rate*10
+        else:
+            reward = -(agent.rate*2+(agent.required))
+        return reward - max(self.load, 0)
 
+    def random_data(self):
+            df = pd.read_csv(self.data_path)
+            df = df.sort_values('Datetime').reset_index(drop=True)
+            index = random.randint(1,len(df))
+            entry = df.iloc[index]
+            while (not ("00:00:00" in entry["Datetime"])):
+                index = random.randint(1,len(df))
+                entry = df.iloc[index]
+                pass
+            self.energy_cost = []
+            for count in range(index, index+24):
+                self.energy_cost.append(float(df.iloc[count]["PJME_MW"]))
+
+            amin, amax = min(self.energy_cost), max(self.energy_cost)
+            for i, val in enumerate(self.energy_cost):
+                self.energy_cost[i] = (val-amin) / (amax-amin)
+
+            self.energy_cost = [ i + .5 for i in self.energy_cost]
+            pass
 
         
     def smart_buildings(self, agent, world):
